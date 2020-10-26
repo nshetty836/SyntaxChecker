25f52bb05f74e52dab8f1407a47fdd39ad17ee48
Nikita Shetty <nshetty@chapman.edu>
2020-10-26 08:08:19 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,30 +4,48 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-  // if(argc >= 2)
-  // {
-  //   string fileName = argv[1];
-  // }
-  // else
-  //   cout << "\nERROR: Please give a data file name in the command line.\n" << endl;
+  //checking if there at least 2 arguments in command line to get file name
+  if(argc >= 2)
+  {
+    string fileName = argv[1];
+    SyntaxChecker *s1;
+    int choice;
+    //while loop while the user wants to check more files
+    while(true){
+      s1 = new SyntaxChecker(fileName);
+      //checking whether the file can be opened
+      if(s1->checkFile() == true){
+        bool check = s1->checkForMatch();
+        //if all delimiters match, ask user if they want to check another or exit
+        if(check == true){
+          cout << "\nDelimiter syntax in file '" << fileName << "' is correct." << endl;
+          cout <<  "\n1) Check another file\n2) Exit\nEnter 1 or 2: " << endl;
+          cin >> choice;
+          if(choice == 1){
+            cout << "Enter the file name: " << endl;
+            cin >> fileName;
+            continue;
+          }
+          else{
+            cout << "Thank you, good bye.\n" << endl;
+            break;
+          }
+        }
+        else
+          break;
+      }
+      else{
+        cout << "\nError: file could not be opened. \nPlease enter file name: " << endl;
+        cin >> fileName;
+      }
+    }
+  }
+  //if there is no command line argument
+  else
+    cout << "\nError: Please give a source code file name in the command line.\n" << endl;
 
 
-  SyntaxChecker *s1 = new SyntaxChecker("sampleinput.txt");
-  s1->checkForMatch();
 
 
-
-  // Stack<char> s1(2);
-  //
-  // s1.push('R');
-  // s1.push('i');
-  // s1.push('c');
-  // // s1.push('e');
-  //
-  // // cout << s1.peek() << endl;
-  // // cout << s1.pop() << endl;
-  // // cout << s1.peek() << endl;
-  // cout << s1.getSize() << endl;
-  // s1.printStack();
 
 }
